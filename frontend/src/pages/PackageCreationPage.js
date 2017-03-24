12e7010781050d258fb5dcedd16aed457d18533e
Daniel Sram <sramdaniel@gmail.com>
2017-03-24 11:27:07 +0100

    add routes and blank pages

---
+++ frontend/src/pages/PackageCreationPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class PackageCreationPage extends React.Component {
+  render() {
+    return (
+      <div>
+        Package creation
+      </div>
+    );
+  }
+}
+
