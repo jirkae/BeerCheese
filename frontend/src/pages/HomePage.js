237d7addd076b42b41586f509a5a76d73b0ee6ea
jirkae <jirka.endrst@gmail.com>
2017-04-12 08:58:31 +0200

    Hotfix (#17)
    * Uprava routovani/struktury prehledu balicku
    
    * Package overview layout
    
    * refactoring package overview
    
    * hotfix let -> const
    
    * package overview nav prev/next
    
    * updates
    
    * hotfix

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -48,7 +48,7 @@ export default class HomePage extends Component {
     return (
       <Row>
         <WelcomeWarningPopUp />
-        <Col xl="2" lg="2" md="4" sm="12" xs="12">
+        <Col xl="3" lg="3" md="4" sm="12" xs="12">
           <Nav pills vertical>
             <NavbarBrand>{localizedTexts.HomePage.categories}</NavbarBrand>
             <NavItem>
@@ -82,7 +82,7 @@ export default class HomePage extends Component {
             </NavItem>
           </Nav>
         </Col>
-        <Col xl="10" lg="10" md="8" sm="12" xs="12" style={{paddingTop: '50px'}}>
+        <Col xl="9" lg="9" md="8" sm="12" xs="12" style={{paddingTop: '50px'}}>
           <ProductList itemSize="250"/>
           <Button>{localizedTexts.HomePage.previous}</Button>
           <Button>{localizedTexts.HomePage.next}</Button>
